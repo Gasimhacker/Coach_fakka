2a00f50fce67f46eb694e8556a17e93bdd0eb2c8
Obada A.Albadee <obadabadee3@hotmail.com>
2024-05-28 15:25:24 +0300

    Fix imports

--- app/coach_fakka_app/lib/views/main_screen.dart
+++ app/coach_fakka_app/lib/views/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:coach_fakka_app/utils/colors.dart';
+import 'package:coach_fakka_app/views/register_views/register_screens.dart';
 import 'package:flutter/material.dart';
 
 class MainRegister extends StatefulWidget {
@@ -17,81 +18,112 @@ class _MainRegisterState extends State<MainRegister> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage(
-                'assets/images/moe_salah.jpg',
-              ),
-            ),
+            AppAvatarWidget(),
             //Coach Fakka Text Widget
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text('Coach Fakka ',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'BungeeSpice')),
-            ),
+            AppTitleWidget(),
             //Are you? Text Widget
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                'Are you?',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Jaro'),
-              ),
-            ),
+            MainScreenTextsWidget('Are you?'),
             //Coach Button
-            Container(
-              width: MediaQuery.of(context).size.width - 80.0,
-              height: 40.0,
-              decoration: BoxDecoration(
-                color: mainColor,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: TextButton(
-                onPressed: () {
-                  Null;
-                },
-                child: Text(
-                  'Coach',
-                  style: TextStyle(color: Colors.white, fontFamily: 'Coiny'),
-                ),
-              ),
-            ),
+            CoachButton(),
             //Or Text Widget
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                'Or',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Jaro'),
-              ),
-            ),
+            MainScreenTextsWidget('Or'),
             //Trainee Button
-            Container(
-              width: MediaQuery.of(context).size.width - 80.0,
-              height: 40.0,
-              decoration: BoxDecoration(
-                color: secondaryColor,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: TextButton(
-                onPressed: () {
-                  Null;
-                },
-                child: Text(
-                  'Trainee',
-                  style: TextStyle(color: Colors.white, fontFamily: 'Coiny'),
-                ),
-              ),
-            ),
+            ClientButton(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ClientButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 80.0,
+      height: 40.0,
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return ClientLogin();
+          }));
+        },
+        child: Text(
+          'Trainee',
+          style: TextStyle(color: Colors.white, fontFamily: 'Coiny'),
+        ),
+      ),
+    );
+  }
+}
+
+class CoachButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 80.0,
+      height: 40.0,
+      decoration: BoxDecoration(
+        color: mainColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return CoachLogin();
+          }));
+        },
+        child: Text(
+          'Coach',
+          style: TextStyle(color: Colors.white, fontFamily: 'Coiny'),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class MainScreenTextsWidget extends StatelessWidget {
+  String _text;
+  MainScreenTextsWidget(this._text);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(
+        _text,
+        style: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Jaro'),
+      ),
+    );
+  }
+}
+
+class AppTitleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text('Coach Fakka ',
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'BungeeSpice')),
+    );
+  }
+}
+
+class AppAvatarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 50,
+      backgroundImage: AssetImage(
+        'assets/images/moe_salah.jpg',
       ),
     );
   }
