9ccc0eaf0fc6e0b3f78d3c60114667b771f95602
Omar-Algassim <omarggg2@gamil.com>
2024-05-28 21:07:04 +0300

    close database whene close register blue print

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,28 @@
 """app for api"""
-
+from models import storage
 from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from flask_cors import CORS
 
 
-app = flask(__name__)
+app = Flask(__name__)
+app.register_blueprint(app_views)
 cros = CORS(app, resources={r"app/v1/*": {"origins": "*"}})
 
-@app_views.errorhandler(404)
+@app.errorhandler(404)
 def not_found(error):
     """error 404"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-# @app_views.teardown_appcontext(error)
-# def tearodown(error):
-#     """for now it will be lik airbnb"""
-#     storage.close()
+@app.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+def status():
+    """return status"""
+    return jsonify({"status": "OK"})
+
+@app.teardown_appcontext
+def tearodown(error):
+    """for now it will be lik airbnb"""
+    storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True) 
