3643ed21bb1694477e216c67ef45f6b5950a841c
Omar-Algassim <omarggg2@gamil.com>
2024-05-28 23:02:15 +0300

    import coach file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.clien import *
 from api.v1.views.exercise import *
 from api.v1.views.workout import *
+from api.v1.views.coach import *
