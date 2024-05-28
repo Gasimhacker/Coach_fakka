558fe4e57b33bdfc28012d0213bcf2d137bd68ec
Omar-Algassim <omarggg2@gamil.com>
2024-05-28 23:02:48 +0300

    impelement client api

--- api/v1/views/clien.py
+++ api/v1/views/clien.py
@@ -1,22 +1,37 @@
+#!/usr/bin/python3
 """client views module"""
 
 from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.client import Client
+from models.coach import Coach
 from models.workout import Workout
+
+client_attrs = ['email', 'age', 'gender', 'weight', 'height',
+                'goal_weight', 'training_place', 'training_days',
+                'training_hours', 'activity_level', 'need_supplements',
+                'injury_history']
 
 @app_views.route('/<coach_id>/clients', methods=['GET', 'POST'], strict_slashes=False)
 def get_clints(coach_id):
     """get all clients"""
     if request.method == 'GET':
-        clients = storage.all(Client, coach_id)
+        coach = storage.get(Coach, coach_id)
+        if coach is None:
+            abort(404, "Coach Not found")
+        clients = storage.all(Client)
         return jsonify([client.to_dict() for client in clients.values()])
+
     if request.method == 'POST':
+        coach = storage.get(Coach, coach_id)
+        if coach is None:
+            abort(404, "Coach Not found")
         if not request.json:
             abort(400, 'Not a JSON')
-        if 'name' not in request.json:
-            abort(400, 'Missing name')
+        for attr in client_attrs:
+            if attr not in request.json:
+                abort(400, 'Missing Attribute')
         client = Client(**request.json)
         client.coach_id = coach_id
         client.save()
@@ -30,12 +45,21 @@ def get_client(client_id):
         if client is None:
             abort(404)
         return jsonify(client.to_dict())
+
     if request.method == 'PUT':
         if not request.json:
             abort(400, 'Not a JSON')
+        ignore = ['id', 'created_at', 'updated_at']
+        data = request.json
         client = storage.get(Client, client_id)
-        client(**request.json).save()
+        if not client:
+            abort(404, "Client Not found")
+        for k, v in data.items():
+            if k not in ignore:
+                setattr(client, k, v)
+                client.save()
         return jsonify(client.to_dict()), 201
+
     if request.method == 'DELETE':
         client = storage.get(Client, client_id)
         if client is None:
