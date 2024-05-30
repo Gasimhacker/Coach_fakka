b5b50e437466a0b1cc4b4a0577b2f5b5e0d88c25
Omar-Algassim <omarggg2@gamil.com>
2024-05-30 23:55:40 +0300

    fix the client file name

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.clien import *
+from api.v1.views.client import *
 from api.v1.views.exercise import *
 from api.v1.views.workout import *
 from api.v1.views.coach import *
