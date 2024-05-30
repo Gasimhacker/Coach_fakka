8e09a10ce4298e4c324edb7efd47ff630a22ad55
Omar-Algassim <omarggg2@gamil.com>
2024-05-30 23:54:50 +0300

    add error handler 400

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,10 +10,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cros = CORS(app, resources={r"app/v1/*": {"origins": "*"}})
 
+@app.errorhandler(400)
 @app.errorhandler(404)
 def not_found(error):
     """error 404"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    code = str(error).split()[0]
+    description = str(error).split(":")[1]
+    return jsonify({"error": description}), code
 
 @app.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
