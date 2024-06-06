bfb68313811a5efe807993154d31286931f86234
Obada A.Albadee <obadabadee3@hotmail.com>
2024-06-06 10:29:49 +0300

    Create Screens

---
+++ app/coach_fakka_app/lib/views/workout_view/exercise_details_screen.dart
@@ -0,0 +1,2 @@
+//TODO: Implement the exercise details screen when pressed on a certain exercise
+//TODO: By Gassim
--- app/coach_fakka_app/r.dart
+++ app/coach_fakka_app/r.dart
@@ -1,14 +1,29 @@
+import 'package:coach_fakka_app/controllers/network_controllers/coach_api_handler.dart';
 import 'package:coach_fakka_app/controllers/network_controllers/workout_api_handler.dart';
+import 'package:coach_fakka_app/models/client_model.dart';
+import 'package:coach_fakka_app/models/coach_model.dart';
 import 'package:coach_fakka_app/models/workout_model.dart';
-import 'package:flutter/material.dart';
 
 void main() async {
   String coachId = '3e2cafa9-6895-47c6-967c-b9048a97a5d8';
   String AhmdId = 'f5e23672-b827-49bd-a7ca-bd0d0d056872';
 
-  List<WorkoutModel> cw = await WorkoutAPIHandler.getMyWorkouts(AhmdId);
+  CoachModel currentCoach = await CoachApiHandler.getCoach(coachId);
+  print('Current Coach');
+  print(currentCoach.toJson());
 
-  cw.forEach((element) {
-    print(element.name);
+  List<ClientModel> trainees = await CoachApiHandler.getMyClients(coachId);
+
+  print('My Trainees');
+  trainees.forEach((client) {
+    print(client.toJson());
+  });
+
+  print('My Workouts');
+  List<WorkoutModel> cw = await WorkoutAPIHandler.getMyWorkouts(AhmdId);
+  cw.forEach((workout) {
+    print(workout.toJson());
   });
 }
+// Run this file to see the output
+// after you cd into the application directory run using the command: dart run r.dart
